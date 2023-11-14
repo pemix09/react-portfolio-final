c13e4dd91d5a213901262f5c30823ab7b21cb747
pemix09 <pemix09@onet.pl>
2023-11-14 15:32:14 +0100

    Added styled component for skills list

--- src/features/sections/introduction/components/skills.tsx
+++ src/features/sections/introduction/components/skills.tsx
@@ -1,23 +1,43 @@
-import styles from "../introduction.module.css";
+import styled, { css } from "styled-components";
+
+const SkillsComponent = styled.div<{ isActive: boolean }>`
+  transition: 1s ease;
+  margin-top: 20px;
+
+  ${(props) =>
+    props.isActive === true && css`
+    transform: translateX(0)
+  `}
+
+  ${(props) => props.isActive === false && css`
+    transform: translateX(100vw)
+  `}
+`;
+
+const SkillsList = styled.div`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+`;
+
+const Skill = styled.p`
+  font-style: italic;
+  margin: 0;
+`;
 
 export function Skills(props: { isActive: boolean }) {
-  const skillsStyle: React.CSSProperties = {
-    transition: "1s ease",
-    transform: props.isActive ? "translateX(0)" : "translateX(-100vw)",
-    marginTop: "20px",
-  };
-
   return (
-    <div style={skillsStyle}>
+    <SkillsComponent {...props}>
       <h3>My technology stack: </h3>
 
-      <div className={styles.skillsList}>
-        <p className={styles.skill}>C#/.NET: ⭐⭐⭐⭐</p>
-        <p className={styles.skill}>React: ⭐⭐⭐⭐</p>
-        <p className={styles.skill}>JavaScript/TypeScript: ⭐⭐⭐⭐</p>
-        <p className={styles.skill}>Node: ⭐⭐⭐⭐</p>
-        <p className={styles.skill}>Flutter: ⭐⭐</p>
-      </div>
-    </div>
+      <SkillsList>
+        <Skill>C#/.NET: ⭐⭐⭐⭐</Skill>
+        <Skill>React: ⭐⭐⭐⭐</Skill>
+        <Skill>JavaScript/TypeScript: ⭐⭐⭐⭐</Skill>
+        <Skill>Node: ⭐⭐⭐⭐</Skill>
+        <Skill>Flutter: ⭐⭐</Skill>
+      </SkillsList>
+    </SkillsComponent>
   );
 }
